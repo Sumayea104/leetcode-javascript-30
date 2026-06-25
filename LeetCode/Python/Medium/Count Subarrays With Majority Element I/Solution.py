c88f50381bda2bea4f258975928b3d4bc8344b5a
Sumayea Rahman <shorbonam104@gmail.com>
2026-06-25 06:35:13 +0600

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/Count Subarrays With Majority Element I/Solution.py
+++ LeetCode/Python/Medium/Count Subarrays With Majority Element I/Solution.py
@@ -1,3 +1,20 @@
 class Solution:
     def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        total_subarrays = 0
+        n = len(nums)
         
+        for i in range(n):
+            target_count = 0
+            
+            
+            for j in range(i, n):
+                if nums[j] == target:
+                    target_count += 1
+                
+                subarray_length = j - i + 1
+                
+                
+                if target_count > subarray_length // 2:
+                    total_subarrays += 1
+                    
+        return total_subarrays
